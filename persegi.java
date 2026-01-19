b09deb07823cc6aa032bc476af999d698e0680f1
Nabillhandika <nabillhandika57@gmail.com>
2026-01-19 09:52:34 +0700

    Add files via upload

---
+++ persegi.java
@@ -0,0 +1,11 @@
+public class persegi {
+    public static void main(String[] args) {
+        int sisi = 5;
+        for (int i = 0; i < sisi; i++) {
+            for (int j = 0; j < sisi; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
